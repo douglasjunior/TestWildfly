16f42031ca959debfe379be72e545dd00daee9f4
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2016-04-22 14:09:53 -0300

    Adicionado exemplo de tarefa agendada

---
+++ src/main/java/io/github/douglasjunior/testwildfly/task/ProductJob.java
@@ -0,0 +1,26 @@
+package io.github.douglasjunior.testwildfly.task;
+
+import io.github.douglasjunior.testwildfly.model.Product;
+import io.github.douglasjunior.testwildfly.model.ProductDao;
+import java.util.List;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author douglas
+ */
+@Singleton
+public class ProductJob {
+
+    @Inject
+    private ProductDao dao;
+
+    @Schedule(hour = "*", minute = "*", second = "*/5", persistent = false)
+    public void task() {
+        List<Product> result = dao.listAll();
+        System.out.println("Task products: " + result);
+    }
+
+}
